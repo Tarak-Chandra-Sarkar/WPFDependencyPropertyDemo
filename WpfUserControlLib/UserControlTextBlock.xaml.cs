7731f15346436ed5284fb34ba48db457acb85567
Tarak-Chandra-Sarkar <tarak.chandra.sarkar@hotmail.com>
2018-11-15 20:50:19 +0530

    Initial commit

---
+++ WpfUserControlLib/UserControlTextBlock.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfUserControlLib
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControlTextBlock : UserControl
+    {
+        public UserControlTextBlock()
+        {
+            InitializeComponent();
+        }
+
+        public string DependentText
+        {
+            get { return (string)GetValue(DependentTextProperty); }
+            set { SetValue(DependentTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for DependentText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DependentTextProperty =
+            DependencyProperty.Register("DependentText", typeof(string), typeof(UserControlTextBlock), new PropertyMetadata("TestValue"));
+        
+    }
+}
